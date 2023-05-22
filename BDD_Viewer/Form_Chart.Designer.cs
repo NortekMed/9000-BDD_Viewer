3541f01032805e885010d44c3063f81f734d78ba
Nicolas <nicolas.vartanian@nortekgroup.com>
2023-05-22 15:04:11 +0000

    New Features
    - Click graph : Tooltip
    - Save Graph to PNG

--- BDD_Viewer/Form_Chart.Designer.cs
+++ BDD_Viewer/Form_Chart.Designer.cs
@@ -33,21 +33,26 @@
             this.listBox_Y = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // formsPlot1
             // 
             this.formsPlot1.Location = new System.Drawing.Point(135, 12);
             this.formsPlot1.Name = "formsPlot1";
-            this.formsPlot1.Size = new System.Drawing.Size(1048, 531);
+            this.formsPlot1.Size = new System.Drawing.Size(1263, 708);
             this.formsPlot1.TabIndex = 0;
+            this.formsPlot1.LeftClicked += new System.EventHandler(this.formsPlot1_LeftClicked);
+            this.formsPlot1.Load += new System.EventHandler(this.formsPlot1_Load);
             // 
             // listBox_Y
             // 
             this.listBox_Y.FormattingEnabled = true;
             this.listBox_Y.Location = new System.Drawing.Point(12, 66);
             this.listBox_Y.Name = "listBox_Y";
-            this.listBox_Y.Size = new System.Drawing.Size(117, 433);
+            this.listBox_Y.Size = new System.Drawing.Size(117, 277);
             this.listBox_Y.TabIndex = 1;
             this.listBox_Y.SelectedIndexChanged += new System.EventHandler(this.listBox_Y_SelectedIndexChanged);
             // 
@@ -69,11 +74,33 @@
             this.label2.TabIndex = 3;
             this.label2.Text = "AxeName";
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 350);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(117, 72);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Actions";
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(7, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Enregistrer Graphique";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form_Chart
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1195, 555);
+            this.ClientSize = new System.Drawing.Size(1410, 732);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.listBox_Y);
@@ -83,6 +110,7 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Chart";
             this.Load += new System.EventHandler(this.Form_Chart_Load);
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -94,5 +122,7 @@
         private System.Windows.Forms.ListBox listBox_Y;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
--- BDD_Viewer/Form_Chart.cs
+++ BDD_Viewer/Form_Chart.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -11,11 +12,19 @@ using System.Windows.Forms;
 
 using FirebirdSql.Data.FirebirdClient;
 using ScottPlot;
+using ScottPlot.Plottable;
+using ScottPlot.Renderable;
 
 namespace BDD_Viewer
 {
     public partial class Form_Chart : Form
     {
+        private ScottPlot.Plottable.ScatterPlot MyScatterPlot;
+        private ScottPlot.Plottable.MarkerPlot HighlightedPoint;
+        private int LastHighlightedIndex = -1;
+
+        Tooltip last_tooltip = new Tooltip();
+
         public DataTable data;
 
         public double[] xAxis_DateTime;
@@ -103,10 +112,73 @@ namespace BDD_Viewer
         private void listBox_Y_SelectedIndexChanged(object sender, EventArgs e)
         {
             formsPlot1.Plot.Clear();
-            formsPlot1.Plot.AddScatter(xAxis_DateTime, yAxis[listBox_Y.SelectedIndex]);
+            MyScatterPlot = formsPlot1.Plot.AddScatter(xAxis_DateTime, yAxis[listBox_Y.SelectedIndex], label: listBox_Y.SelectedItem.ToString());
             formsPlot1.Plot.XAxis.DateTimeFormat(true);
+
+            formsPlot1.Plot.Title(listBox_Y.SelectedItem.ToString() + " --- " + this.Text);
+            formsPlot1.Plot.XLabel(this.label2.Text);
+            formsPlot1.Plot.YLabel(listBox_Y.SelectedItem.ToString());
+            formsPlot1.Plot.Legend();
+
+            // Add a red circle we can move around later as a highlighted point indicator
+            HighlightedPoint = formsPlot1.Plot.AddPoint(0, 0);
+            HighlightedPoint.Color = Color.Red;
+            HighlightedPoint.MarkerSize = 10;
+            HighlightedPoint.MarkerShape = ScottPlot.MarkerShape.openCircle;
+            HighlightedPoint.IsVisible = false;
+
             formsPlot1.Update();
             formsPlot1.Refresh();
+
+            button1.Enabled= true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(listBox_Y.SelectedItem!= null)
+            {
+                SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+                saveFileDialog1.Filter = "fichiers png (*.png)|*.png";
+                saveFileDialog1.FileName = listBox_Y.SelectedItem.ToString() + " --- " + this.Text;
+                saveFileDialog1.RestoreDirectory = true;
+
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    string path = saveFileDialog1.FileName;
+                    Bitmap bmp_graph = formsPlot1.Plot.Render();
+                    formsPlot1.Render();
+                    formsPlot1.DrawToBitmap(bmp_graph, formsPlot1.ClientRectangle);
+                    bmp_graph.Save(path);
+                }
+            }
+        }
+
+        private void formsPlot1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void formsPlot1_LeftClicked(object sender, EventArgs e)
+        {
+            // determine point nearest the cursor
+            (double mouseCoordX, double mouseCoordY) = formsPlot1.GetMouseCoordinates();
+            double xyRatio = formsPlot1.Plot.XAxis.Dims.PxPerUnit / formsPlot1.Plot.YAxis.Dims.PxPerUnit;
+            (double pointX, double pointY, int pointIndex) = MyScatterPlot.GetPointNearest(mouseCoordX, mouseCoordY, xyRatio);
+
+            // place the highlight over the point of interest
+            HighlightedPoint.X = pointX;
+            HighlightedPoint.Y = pointY;
+            HighlightedPoint.IsVisible = true;
+
+            // render if the highlighted point chnaged
+            if (LastHighlightedIndex != pointIndex)
+            {
+                LastHighlightedIndex = pointIndex;
+                formsPlot1.Plot.Remove(last_tooltip);
+                last_tooltip = formsPlot1.Plot.AddTooltip(yAxis[listBox_Y.SelectedIndex][pointIndex].ToString(), pointX,pointY);
+                formsPlot1.Render();
+            }
         }
     }
 }
